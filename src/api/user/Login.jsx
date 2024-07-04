668462773174b5ad203520f25622033493a33a2a
박현민 <dndb4902@naver.com>
2024-07-04 11:47:31 +0900

    # deploy test

--- src/api/user/Login.jsx
+++ src/api/user/Login.jsx
@@ -2,9 +2,10 @@ import axios from "axios"
 import { KAKAO_AUTH_URL, KAKAO_TOKEN_URL } from "../oauth/Oauth"
 
 const Login = () => {
+    console.log(import.meta.env.VITE_SOCKET_SERVER_URL);
     return (
         <div>
-            로그인 페이지
+            로그인 페이지111
             <button onClick={() => { window.location.href = KAKAO_AUTH_URL }}>
                 로그인 하기
             </button>

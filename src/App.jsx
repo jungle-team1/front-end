44c366f6b1ced15abc2c8188303a4bdab721259c
박현민 <dndb4902@naver.com>
2024-07-04 11:38:28 +0900

    # websocket simultaneous editing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from "./api/user/Login"
+import Canvas from "./pages/Canvas.jsx";
+import useSocketStore from "./store/useSocketStore.tsx";
+import {useEffect, useRef} from "react";
+import {io, Socket} from "socket.io-client";
+
+
 function App() {
+    const {socket, setSocket} = useSocketStore();
+    const socketRef = useRef<Socket | null>(null);
+
+    useEffect(() => {
+        const socketConnect = io(import.meta.env.VITE_SOCKET_SERVER_URL);
+        setSocket(socketConnect);
+        // socketRef.current = socketConnect;
+        //
+        // socketRef.current.on("connect",() => {
+        //     socketRef.current.emit("connenct_check", "ㅎㅇㅎㅇ");
+        // });
+        //
+        // return () => {
+        //     socketRef.current.disconnect();
+        // };
+
+
+        socketConnect.on("connect",() => {
+            socketConnect.emit("connenct_check", "ㅎㅇㅎㅇ");
+        });
+
+        return () => {
+            socketConnect.disconnect();
+        };
+    }, [setSocket]);
+
+
+
   return (
     <>
       <Routes>
         <Route path='/login' element={<Login />} />
+        <Route path='/test' element={<Canvas />} />
       </Routes>
     </>
   )
---
+++ src/socket/socket.ts
@@ -0,0 +1,45 @@
+// import { io, Socket } from "socket.io-client";
+//
+// class SocketService {
+//     private readonly socket: Socket<ServerToClientEvents, ClientToServerEvents> =
+//         io(socketUrl, {
+//             autoConnect: false,
+//         });
+//
+//     connectWithAuthToken(token: string) {
+//         this.socket.auth = { token };
+//         this.socket.connect();
+//     }
+//
+//     disconnect() {
+//         this.socket.disconnect();
+//     }
+//
+//     sendMessage(data: AddMessageDto) {
+//         this.socket.emit("message", data);
+//     }
+//
+//     notifyTyping(roomId: number) {
+//         this.socket.emit("isTyping", roomId);
+//     }
+//
+//     subscribeToMessages(messageHandler: ServerToClientEvents["message"]) {
+//         this.socket.on("message", messageHandler);
+//     }
+//
+//     subscribeToTypingNotifications(
+//         typingNotificationsHandler: ServerToClientEvents["isTyping"]
+//     ) {
+//         this.socket.on("isTyping", typingNotificationsHandler);
+//     }
+//
+//     joinRoom(roomId: number) {
+//         this.socket.emit("join", roomId);
+//     }
+//
+//     leaveRoom(roomId: number) {
+//         this.socket.emit("leave", roomId);
+//     }
+// }
+//
+// export const socketService = new SocketService();

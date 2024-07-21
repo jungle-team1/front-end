25de15b85e944783fa665081c49be07a98e86358
Chaeyun06 <kilskg@gmail.com>
2024-07-22 02:35:34 +0900

    temp commit

---
+++ src/pages/Test.jsx
@@ -0,0 +1,18 @@
+import './Test.css'
+
+const Test = () => {
+  return(
+<div class="card">
+  <img
+    src="https://uiverse.io/build/_assets/astronaut-WTFWARES.png"
+    alt=""
+    className="image"
+  />
+  <div class="icons">
+  </div>
+</div>
+
+  );
+}
+
+export default Test;
